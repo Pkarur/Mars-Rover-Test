af16cbc971bb89b35455366a60f6a8b92be354be
Pkarur <karurp@gmail.com>
2020-09-02 17:50:33 +0530

    Add files via upload

---
+++ MarsRover/Constants.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarsRover
+{
+    /// <summary>
+    /// Necessary constants for application.
+    /// Purpose: To get rid of Magic Strings.
+    /// </summary>
+    public static class Constants
+    {
+        /// <summary> 
+        /// Constants for Debug Messages
+        /// </summary>
+        public static class DebugMessages
+        {
+            public const string START_APP = "Start Application !!!";
+            public const string DONE_APP = "All Done !!!";
+        }
+
+        /// <summary>
+        /// Constans for User Friendly Messages
+        /// </summary>
+        public static class UserFriendlyMessages
+        {
+            public const string ENTER_MOTION_ENTRIES = "Enter the motion entries :";
+        }
+    }
+}
---
+++ MarsRover/Models/Position.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static MarsRover.Models.Enums;
+
+namespace MarsRover.Models
+{
+    /// <summary>
+    /// Entity that stores Rover's location
+    /// </summary>
+    public class Position
+    {
+        public Position()
+        {
+            X = 1;
+            Y = 2;
+            Directory = Direction.N;
+        }
+
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public Direction Directory { get; set; }
+    }
+}
